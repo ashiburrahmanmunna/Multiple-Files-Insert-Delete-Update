5fe757ccffe786980490e81d35d0c95f201e1b93
Ashibur Rahman Munna <114874876+ashiburrahmanmunna@users.noreply.github.com>
2023-03-18 19:54:15 +0600

    Add files via upload

---
+++ MultiplefileApp/MultiplefileForm.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MultiplefileForm.aspx.cs" Inherits="MultiplefileApp.MultiplefileForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table align="center">
+            <tr>
+                <td>ID</td>
+                <td><asp:TextBox id="ID" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td>Name</td>
+                <td><asp:TextBox id="Name" runat="server"></asp:TextBox></td>
+            </tr>
+
+             <tr>
+                
+                <td><asp:FileUpload id="FileUpload1" AllowMultiple="true" runat="server"/> </td>
+            </tr>
+
+            <tr>
+                <td><asp:Button id="btnInsert" runat="server" Text="Insert" OnClick="btnInsert_Click" /></td>
+                <td><asp:Button id="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" /></td>
+                <td><asp:Button id="btnUpdate" runat="server" Text="Update" OnClick="btnUpdate_Click" /></td>
+            </tr>
+        </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ MultiplefileApp/MultiplefileForm.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Configuration;
+
+
+namespace MultiplefileApp
+{
+    public partial class MultiplefileForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-ML3UPSH\\SQLEXPRESS;Initial Catalog=MultiplefileApp;Persist Security Info=True;User ID=sa;Password=Munna12345");
+
+            //SqlCommand cmd = new SqlCommand(@"INSERT INTO [dbo].[MultiplefileApp] ([ID] ,[Name], [Multiplefile])     
+            //VALUES('" + ID.Text+ "', '"+Name.Text+ "', '"+ FileUpload1+"')", con); 
+
+            SqlCommand cmd = new SqlCommand("INSERT INTO MultiplefileApp(ID,Name,Multiplefile) VALUES(@ID,@Name,@FileUpload1)", con);
+
+            con.Open();
+            
+            Response.Write("Data inserted successfully");
+            
+
+            if (FileUpload1.HasFiles)
+            {
+                foreach (HttpPostedFile file in FileUpload1.PostedFiles)
+                {
+                    cmd.Parameters.AddWithValue("@ID", ID.Text);
+                    cmd.Parameters.AddWithValue("@Name", Name.Text);
+                    cmd.Parameters.AddWithValue("@FileUpload1", file.FileName);
+                    file.SaveAs(Path.Combine(Server.MapPath("/Files/"), file.FileName));
+                    
+                }
+            }
+            
+            cmd.ExecuteNonQuery();
+            cmd.Parameters.Clear();
+            con.Close();
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-ML3UPSH\\SQLEXPRESS;Initial Catalog=MultiplefileApp;Persist Security Info=True;User ID=sa;Password=Munna12345");
+            SqlCommand cmd = new SqlCommand(@"DELETE FROM [dbo].[MultiplefileApp]
+            WHERE [ID]='"+ID.Text+ "'",con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            Response.Write("Data deleted successfully");
+            con.Close();
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-ML3UPSH\\SQLEXPRESS;Initial Catalog=MultiplefileApp;Persist Security Info=True;User ID=sa;Password=Munna12345");
+            SqlCommand cmd = new SqlCommand(@"UPDATE [dbo].[MultiplefileApp]   
+            SET [ID] = '"+ID.Text+ "'  ,[Name] = '"+Name.Text+ "' , [Multiplefile]='"+ FileUpload1+"'  WHERE [ID]= '" + ID.Text+"'",con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            Response.Write("Data Updated successfully");
+            con.Close();
+        }
+    }
+}
